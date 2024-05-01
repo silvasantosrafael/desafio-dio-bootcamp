84dc57e302447bb59cd13bff98034183aa28c3bd
silvasantosrafael <rafael.silva318819@gmail.com>
2024-04-30 21:59:33 -0300

    Initial commmit

---
+++ src/main/java/br/com/dio/challenge/Main.java
@@ -0,0 +1,7 @@
+package br.com.dio.challenge;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/br/com/dio/challenge/domain/Bootcamp.java
@@ -0,0 +1,11 @@
+package br.com.dio.challenge.domain;
+
+import java.time.LocalDate;
+
+public class Bootcamp {
+    private String name;
+    private String description;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+}
